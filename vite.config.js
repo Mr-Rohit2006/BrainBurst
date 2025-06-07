838b61fd3612082826e2957c61421e9365cb4614
Rohit Goyal <163022120+Mr-Rohit2006@users.noreply.github.com>
2025-06-07 08:28:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/mr-rohit2006.github.io-BrainBurst/",
   plugins: [react()],
-})
+});
